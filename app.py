5d950cb0dddcdc22e02b683d98756dd2c1524501
agustin diaz <diazagustiin30@gmail.com>
2025-04-28 15:43:50 -0300

    agregamos nuevas rutas para la mini aplicacion

--- app.py
+++ app.py
@@ -5,3 +5,20 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/river")
+def hola_river():
+    return "<p>mal ahi river</p>"
+
+
+
+@app.route("/boca")
+def hola_boca():
+    return "<p>aguante boca </p>"
+
+
+
+@app.route("/SanLorenzo")
+def hola_sanLorenzo():
+    return "<p> san lorenzo </p>"
