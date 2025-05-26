0fceb980272f1ac1e7707061e6fbf89a07320fe3
agustin diaz <diazagustiin30@gmail.com>
2025-05-26 15:28:55 -0300

    termine los url_for

--- app.py
+++ app.py
@@ -1,24 +1,30 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
-
+@app.route("/")
+def hello_wordl():
+    return f""" <p>Hello, World!</p>
+    <a href="{url_for('hola_river')}"> una ruta sencilla</a><br>
+    <a href="{url_for('hola_boca')}"> una ruta sencilla</a><br>
+    <a href="{url_for('hola_sanLorenzo')}"> una ruta sencilla</a><br>
+    <a href="{url_for('partido',e1="boca",e2="river")}"> una ruta sencilla</a><br>
+    
+"""
 
 @app.route("/river")
 def hola_river():
     return "<p>mal ahi river</p>"
 
 
-
 @app.route("/boca")
 def hola_boca():
-    return "<p>aguante boca </p>"
-
+    return "<p>aguante boca</p>"
+    
 
 
 @app.route("/SanLorenzo")
 def hola_sanLorenzo():
     return "<p> san lorenzo </p>"
-
 
 
 
